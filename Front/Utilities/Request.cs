dee62bbb8afe24fedfdba269511a7788bb153001
samueldavidra17 <samueldavidra@gmail.com>
2022-11-29 13:36:37 -0400

    Pre defensa

--- Front/Utilities/Request.cs
+++ Front/Utilities/Request.cs
@@ -17,7 +17,7 @@ namespace Front
 {
     public class Request
     {
-        public static string url = "http://192.168.0.110:3000/api/";
+        public static string url = "http://192.168.1.101:3000/api/";
         public static HttpClient client = new HttpClient();
 
         public static async Task<string> Get(string link)
